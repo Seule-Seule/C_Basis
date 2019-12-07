28d7657b1a1c87f0788a6730e805e91ae9b3aae7
Seule-Seule <soul.seule@qq.com>
2019-12-07 16:34:01 +0000

    BUsy day.

--- Day_Three/test4/test4.c
+++ Day_Three/test4/test4.c
@@ -2,6 +2,8 @@
 #include "stdlib.h"
 #include "stdio.h"
 
+int FreeGetMemory(char ***tmp,int *len);
+
 int SpitStr(const char *str,char *Ctmp,char buf[10][30],int *nCount){
     char *SpStr = str;
     char *strtmp = NULL;
@@ -36,9 +38,11 @@ int SpitStr1(const char *str,char *Ctmp,char ***buf,int *nCount){
     int tmpCount = 0;
     int i = 0;
     int bufCount  = 0;
+    int ErroMsg = 0;
     if (str == NULL || Ctmp == NULL || buf == NULL || nCount == NULL){
         printf("str == NULL || Ctmp == NULL || buf == NULL || nCount = NULL Erro Key : -1");
-        return -1;
+        ErroMsg = -1;
+        goto END;
     }
     while(*SpStr != '\0'){
         SpStr = strstr(SpStr,tmp);
@@ -47,15 +51,17 @@ int SpitStr1(const char *str,char *Ctmp,char ***buf,int *nCount){
     }
     SpStr = str;
     bufLine = (char **)malloc(sizeof(char *) * (bufCount+1));
+    memset(bufLine,0,bufCount+1);
     BufLineTmp = bufLine;
     for (i = 0;i < bufCount;i++){
         strtmp = strstr(SpStr,tmp);
         tmpCount = strtmp - SpStr;
         //strncpy(buf[i],SpStr,tmpCount);
-        BufTmp = (char *)malloc(sizeof(char) * tmpCount);
+        BufTmp = (char *)malloc(sizeof(char) * (1 + tmpCount));
         if (BufTmp == NULL){
             printf("(BufTmp == NULL) Error Key : -2");
-            return -2;
+            ErroMsg = -2;
+            goto END;
         }
         strncpy(BufTmp,SpStr,tmpCount);
         *(BufTmp+tmpCount) = '\0';
@@ -63,10 +69,36 @@ int SpitStr1(const char *str,char *Ctmp,char ***buf,int *nCount){
         strtmp = strtmp+strlen(tmp);
         SpStr = strtmp;
     }
-    *buf = bufLine;
-    *nCount = i;
 
-    return 0 ;
+END:if (ErroMsg != 0){
+        ErroMsg = FreeGetMemory(&bufLine,&bufCount);
+    }
+    else {
+        *buf = bufLine;
+        *nCount = i;
+    }
+
+    return ErroMsg;
+}
+
+int FreeGetMemory(char ***tmp,int *len){
+    int i = 0;
+    int line = *len;
+    char **str = *tmp;
+    if (tmp == NULL){
+        printf("(tmp == NULL) Error key: -1\n");
+        return -1;
+    }
+
+    for (i = 0;i < line;i++){
+        if (*(str+i) != NULL){
+            free(*(str+i));
+            *(str+i) == NULL;
+        }
+    }
+    free(str);
+    *tmp = NULL;
+    return 0;
 }
 
 int main(){
@@ -84,9 +116,10 @@ int main(){
     }
     
     for (i = 0;i < count;i++){
-        printf("%s\n",buf1[i]);
+        printf("'%s'\n",buf1[i]);
     }
-
+    
+    FreeGetMemory(&buf1,&count);
 
     printf("Hello world\n");
     return 0;
